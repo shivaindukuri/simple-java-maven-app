c9096ad3c7031de082e2a7ea4be610a1773b9ea2
Lokesh Kamalay <lokesh.mydilse@gmail.com>
2022-07-23 08:46:23 -0500

    yo

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Bro!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Loki!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Bro!\"");
+            fail("\"message\" is not \"Hello Loki!\"");
         }
     }
 
